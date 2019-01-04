b36b35540f83397a886ef642372b8b64f796b5d9
Rajvel Govindarajan <govinr@ucles.internal>
2019-01-04 17:45:13 +0530

    added some changes

--- firstdjango/settings.py
+++ firstdjango/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'firstdjango.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'TEST',
+        'NAME': 'world',
         'USER' : 'root',
         'PASSWORD' : 'password',
         'HOST' : '127.0.0.1',
--- music/urls.py
+++ music/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     
     # /music/71/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
-
+    
+    # /music/<album_id>/favorite/
+    url(r'^(?P<album_id>[0-9]+)/favorites/$', views.favorite, name='favorite'),
 ]
 
